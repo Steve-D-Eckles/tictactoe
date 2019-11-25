f967976e5d9d55aa4247c5584ed5e9f35d092b82
Kevin Lueke <kevinlueke66@gmail.com>
2019-11-25 15:19:31 -0500

    Add player turn message

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const grid = ['', '', '', '', '', '', '', '', '']
 let turn = 0
 const syms = ['X', 'O']
 const arr = Array.from(document.getElementById('board').children)
+const turnHeading = document.getElementById('turn')
 
+turnHeading.textContent = `It's Player ${syms[turn]}'s turn.`
 
 for (let i = 0; i < 9; i++) {
   arr[i].addEventListener('click', play)
@@ -12,6 +14,7 @@ function play (event) {
   const index = arr.indexOf(event.target)
   event.target.textContent = syms[turn]
   turn = turn === 1 ? 0 : 1
+  turnHeading.textContent = `It's Player ${syms[turn]}'s turn.`
   event.target.removeEventListener('click', play)
 }
 
