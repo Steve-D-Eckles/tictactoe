0b285da137bd9bf18960f5c2b3d1e958c7ac8df2
Steve Eckles <steve.d.eckles@gmail.com>
2019-11-25 15:46:20 -0500

    Add ability to win game

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const grid = ['', '', '', '', '', '', '', '', '']
 let turn = 0
 const syms = ['X', 'O']
 const arr = Array.from(document.getElementById('board').children)
-const turnHeading = document.getElementById('turn')
+const output = document.getElementById('turn')
 
-turnHeading.textContent = `It's Player ${syms[turn]}'s turn.`
+output.textContent = `It's Player ${syms[turn]}'s turn.`
 
 for (let i = 0; i < 9; i++) {
   arr[i].addEventListener('click', play)
@@ -12,10 +12,59 @@ for (let i = 0; i < 9; i++) {
 
 function play (event) {
   const index = arr.indexOf(event.target)
+  grid[index] = syms[turn]
   event.target.textContent = syms[turn]
-  turn = turn === 1 ? 0 : 1
-  turnHeading.textContent = `It's Player ${syms[turn]}'s turn.`
+  if (winCheck(index)) {
+    output.textContent = `Player ${syms[turn]} wins!`
+  } else {
+    turn = turn === 1 ? 0 : 1
+    output.textContent = `It's Player ${syms[turn]}'s turn.`
+  }
   event.target.removeEventListener('click', play)
 }
 
-
+function winCheck (index) {
+  if (index === 0) {
+    if ((grid[0] === grid[1] && grid[0] === grid[2]) ||
+        (grid[0] === grid[3] && grid[0] === grid[6]) ||
+        (grid[0] === grid[4] && grid[0] === grid[8])) return true
+  }
+  if (index === 1) {
+    if ((grid[1] === grid[2] && grid[1] === grid[0]) ||
+        (grid[1] === grid[4] && grid[1] === grid[7])) return true
+  }
+  if (index === 2) {
+    if ((grid[2] === grid[1] && grid[2] === grid[0]) ||
+        (grid[2] === grid[5] && grid[2] === grid[8]) ||
+        (grid[2] === grid[4] && grid[2] === grid[6])) return true
+  }
+  if (index === 3) {
+    if ((grid[3] === grid[4] && grid[3] === grid[5]) ||
+        (grid[3] === grid[0] && grid[3] === grid[6])) return true
+  }
+  if (index === 4) {
+    if ((grid[4] === grid[3] && grid[4] === grid[5]) ||
+        (grid[4] === grid[1] && grid[4] === grid[7]) ||
+        (grid[4] === grid[0] && grid[4] === grid[8]) ||
+        (grid[4] === grid[6] && grid[7] === grid[2])) return true
+  }
+  if (index === 5) {
+    if ((grid[5] === grid[2] && grid[5] === grid[8]) ||
+        (grid[5] === grid[4] && grid[5] === grid[3])) return true
+  }
+  if (index === 6) {
+    if ((grid[6] === grid[3] && grid[6] === grid[0]) ||
+        (grid[6] === grid[7] && grid[6] === grid[8]) ||
+        (grid[6] === grid[4] && grid[6] === grid[2])) return true
+  }
+  if (index === 7) {
+    if ((grid[7] === grid[6] && grid[7] === grid[8]) ||
+        (grid[7] === grid[4] && grid[7] === grid[1])) return true
+  }
+  if (index === 8) {
+    if ((grid[8] === grid[5] && grid[8] === grid[2]) ||
+        (grid[8] === grid[7] && grid[8] === grid[6]) ||
+        (grid[8] === grid[4] && grid[8] === grid[0])) return true
+  }
+  return false
+}
